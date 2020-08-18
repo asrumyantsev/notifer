bf5bb662d6e208250f51d442823bd6497c43edd8
asrumyantsev <asrumyantsev@yahoo.com>
2020-08-18 17:03:26 +0300

    Initial commit

---
+++ src/main/java/com/notifer/notifer/NotiferApplication.java
@@ -0,0 +1,13 @@
+package com.notifer.notifer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotiferApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotiferApplication.class, args);
+    }
+
+}
